701133523a74160f0a26349d2b63a44d02312511
Igor mm <igor.mmiguel@eaportal.org>
2023-10-15 00:01:57 -0300

    ex 1 lista 2

--- fixação.sql
+++ fixação.sql
@@ -183,3 +183,21 @@ END;
 DELIMITER ;
 SELECT verificarPalindromo('Onix');
 SELECT verificarPalindromo('Spacefox'); 
+
+
+-----------------------------------------------------------------------------------------------------------------
+-- lista 2
+
+-- 1
+CREATE OR REPLACE FUNCTION total_livros_por_genero(genre_name text) RETURNS integer AS $$
+DECLARE
+  total_count integer;
+BEGIN
+  total_count := 0;
+  FOR livros IN
+    SELECT * FROM books WHERE genre = genre_name
+  LOOP
+    total_count := total_count + 1;
+  END LOOP;
+  RETURN total_count;
+END;
