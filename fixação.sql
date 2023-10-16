cf09867a3a5c98a8acca69ebd70ca206807bcdf6
Igor mm <igor.mmiguel@eaportal.org>
2023-10-15 21:51:45 -0300

    ex 3

--- fixação.sql
+++ fixação.sql
@@ -48,3 +48,36 @@ FROM produtos;
 --D
 SELECT AVG(preco) AS precos_media
 FROM produtos;
+
+--3
+--A
+CREATE TABLE eventos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data_evento DATETIME
+);
+
+INSERT INTO eventos (data_evento) VALUES
+('2023-03-23 10:25:48'),
+('2010-06-25 11:30:00'),
+('2009-03-15 18:25:59'),
+('2008-10-12 02:45:52'),
+('2025-12-29 00:00:00'),
+('1976-03-23 22:02:24'),
+('2036-02-30 23:55:40'),
+('2012-04-25 15:25:36'),
+('2053-05-20 12:25:13'),
+('2018-09-30 19:26:10'),
+('2028-05-25 18:36:10'),
+('2000-06-02 19:05:32');
+
+--B
+INSERT INTO eventos (evento_data)
+SELECT NOW() as DatAtual;
+
+--C
+SELECT evento_data, evento_data AS data_atual, DATEDIFF(NOW(), evento_data) AS Comparação_total
+FROM eventos;
+
+--D
+SELECT DAYNAME(evento_data) AS nome_dia_semana
+FROM eventos;
